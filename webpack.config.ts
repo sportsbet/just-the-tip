aebfcaf23d08be6819e7c1b8dca73e6c1fe0d64e
Damon Smith <damon.smith@sportsbet.com.au>
2017-09-17 15:26:02 +1000

    feat: adds web framework base
    to use, run:
    yarn run webpack
    yarn run webpack-dev-server

---
+++ webpack.config.ts
@@ -0,0 +1,55 @@
+import * as HtmlWebpackPlugin from "html-webpack-plugin"
+const CleanObsoleteChunks = require("webpack-clean-obsolete-chunks")
+
+const config = {
+	stats: "errors-only",
+	entry: {
+		app: "./src/web_index.tsx"
+	},
+	output: {
+		path: __dirname + "/build/web",
+		filename: "[name]-[hash].js",
+		devtoolModuleFilenameTemplate: "[absoluteResourcePath]",
+		publicPath: "/"
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "web/index.html",
+			filename: "index.html"
+		}),
+		new CleanObsoleteChunks()
+	],
+	resolve: {
+		extensions: [".js", ".ts", ".tsx"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loader: "ts-loader",
+				options: {
+					configFile: "tsconfig.web.json"
+				}
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				loader: "sizeof-loader",
+				options: {
+					limit: "8192",
+					name: "images/[name]-[hash].[ext]"
+				}
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				loader: "file-loader",
+				options: {
+					name: "fonts/[name].[ext]"
+				}
+			}
+		]
+	},
+	devtool: "source-map"
+}
+
+export default config
